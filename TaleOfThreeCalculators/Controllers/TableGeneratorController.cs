a9fa3a16417224fb3ff338da5b2adf3eb8023a61
Ryan Parlin <parlinr@ad.nmc.edu>
2018-01-30 14:57:51 -0500

    added all constructors

---
+++ TaleOfThreeCalculators/Controllers/TableGeneratorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaleOfThreeCalculators.Controllers
+{
+    public class TableGeneratorController : Controller
+    {
+        // GET: TableGenerator
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TaleOfThreeCalculators/Controllers/UrlCalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaleOfThreeCalculators.Controllers
+{
+    public class UrlCalculatorController : Controller
+    {
+        // GET: UrlCalculator
+        public ActionResult Index(string operand1, string operand2)
+        {
+            
+        }
+    }
+}
