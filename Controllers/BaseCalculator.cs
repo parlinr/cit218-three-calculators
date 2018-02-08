c3d26cc1d5cfe13d384e21e8ebe50a0776e26dfd
Ryan Parlin <parlinr@mail.nmc.edu>
2018-02-08 14:28:53 -0500

    changed .NET Core version to 1.1

--- Controllers/BaseCalculator.cs
+++ Controllers/BaseCalculator.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using cit218_three_calculators.Models;
+
 
 namespace cit218_three_calculators.Controllers
 {
--- Startup.cs
+++ Startup.cs
@@ -6,27 +6,37 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace cit218_three_calculators
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -43,11 +53,11 @@ namespace cit218_three_calculators
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-                
+
                 routes.MapRoute(
-                    name: "calculate",
-                    template: "{controller=UrlCalculator}/{action=Calculate}/{operation}/{operand1}/{operand2}");
-                
+                  name: "calculate",
+                  template: "{controller=UrlCalculator}/{action=Calculate}/{operation}/{operand1}/{operand2}"  
+                );
             });
         }
     }
